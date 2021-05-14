7c348c45f3333623c7380e1a584b9297e89ccdfc
Pranit Ingole <61083651+pranitcode@users.noreply.github.com>
2021-05-14 20:21:41 +0530

    5folder

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -29,7 +29,7 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       {
         loading ? (<Loader />)
-        : error ? (<Message varient="danger">{error }</Message>) : (
+        : error ? (<Message varient="danger">{error}</Message>) : (
           <Row>
              {products.map((product) => (
                  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
